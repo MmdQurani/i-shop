55a7cf25eeae1099fed3cdcfa31066d817e49fef
78 mmd 69 <mmdqurani@gmail.com>
2024-11-27 14:26:50 +0330

    Calling the topbar and navbar components in the header component

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Topbar from './Topbar/Topbar'
+import Navbar from './Navbar/Navbar'
+import { Container } from 'react-bootstrap'
+import './Header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className='headerSize'>
+      <Container className='bg-warning p-0 d-flex flex-column h-100'>
+        <Topbar />
+        <Navbar />
+      </Container>
+    </header>
   )
 }
 
