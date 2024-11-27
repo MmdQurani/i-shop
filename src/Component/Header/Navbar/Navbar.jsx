61d72782db8e4b2fbc23403fdc51f64567cb24fe
78 mmd 69 <mmdqurani@gmail.com>
2024-11-27 14:27:52 +0330

    Initial coding of header components

--- src/Component/Header/Navbar/Navbar.jsx
+++ src/Component/Header/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div style={{height:'45%' , backgroundColor:'#000'}}>Navbar</div>
   )
 }
 
--- src/Component/Header/Topbar/Topbar.jsx
+++ src/Component/Header/Topbar/Topbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Topbar() {
   return (
-    <div>Topbar</div>
+    <div className='bg-light topBar'>
+      <nav className='d-flex flex-row justify-content-between h-100'>
+        <div className='left-box-topbar d-flex flex-row justify-content-start align-items-center column-gap-3'>
+          <Link to='/'>
+            <button className='btn bg-btn px-4 py-2 rounded-3'>click left</button>
+          </Link>
+          <Link to='/'>
+            <button className='btn bg-main text-color-main px-4 py-2 rounded-3'>click right</button>
+          </Link>
+        </div>
+        <div style={{ width: '50%', height: '100%', backgroundColor: '#001230' }}></div>
+      </nav>
+    </div>
   )
 }
 
