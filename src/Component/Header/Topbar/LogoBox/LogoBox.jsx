e52d76c4cd99cdd60fe3ec08b74bbad7d332d6dc
78 mmd 69 <mmdqurani@gmail.com>
2024-11-29 15:55:59 +0330

    Converting the buttons in the site logo section from the Topbar section of the site header to a component

---
+++ src/Component/Header/Topbar/LogoBox/LogoBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Image } from 'react-bootstrap';
+
+function LogoBox() {
+  return (
+    <>
+      <div className='h-100 d-flex justify-content-center align-items-center'>
+        <Image src='./public/assets/img/Logo/logo1.jpg' width={150} />
+      </div>
+    </>
+  )
+}
+
+export default LogoBox
