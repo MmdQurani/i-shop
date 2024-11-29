c436e752a124d2e79e58097f493e5a81ec0f7c58
78 mmd 69 <mmdqurani@gmail.com>
2024-11-29 13:25:11 +0330

    Responsive header section for laptop and larger sizes

--- src/Component/Header/Navbar/Navbar.jsx
+++ src/Component/Header/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
           <div className='d-flex justify-content-center align-items-center h-100' style={{ width: 'max-content' }}>
             <span className='fs-4 text-color-two'>شگفت انگیزشو</span>
           </div>
-          <div className='h-100 w-50 d-flex justify-content-center align-items-center flex-row'>
+          <div className='h-100 w-navbar-item d-flex justify-content-center align-items-center flex-row'>
             <div className='w-75 d-flex justify-content-between align-items-center flex-row-reverse'>
               <Link to='/' className='link-st'>صفحه اصلی</Link>
               <Link to='/' className='link-st'>مشخصات قالب</Link>
@@ -21,7 +21,7 @@ function Navbar() {
               <Link to='/' className='link-st'>وبلاگ</Link>
             </div>
             <div className='mx-3' style={{width:'1px' , height:'50%' , backgroundColor:'#1e36bd30'}} />
-            <div className='w-25 d-flex justify-content-center align-items-center'>
+            <div className='d-flex justify-content-center align-items-center'>
               <Link to='/' className='link-st'>دسته بندی محصولات</Link>
             </div>
           </div>
