68e7e98e3195243e93d5527fa895ecc2a654b200
78 mmd 69 <mmdqurani@gmail.com>
2024-11-29 21:14:30 +0330

    Debugging component html tag types

--- src/Component/Header/NavbarBtns/NavbarBtns.jsx
+++ src/Component/Header/NavbarBtns/NavbarBtns.jsx
@@ -5,14 +5,14 @@ function NavbarBtns(prop) {
   return (
     <>
       <div className={`h-100 w-navbar-item ${prop.display} justify-content-center align-items-center flex-row`}>
-        <div className='w-75 d-flex justify-content-between align-items-center flex-row-reverse'>
-          <Link to='/' className='link-st'>صفحه اصلی</Link>
-          <Link to='/' className='link-st'>مشخصات قالب</Link>
-          <Link to='/' className='link-st'>فروشگاه</Link>
-          <Link to='/' className='link-st'>سوالات متداول</Link>
-          <Link to='/' className='link-st'>شگفت انگیزشو</Link>
-          <Link to='/' className='link-st'>وبلاگ</Link>
-        </div>
+          <ul className='w-75 m-0 h-100 d-flex justify-content-between align-items-center flex-row-reverse'>
+            <li className='list-group-item'><Link to='/' className='link-st'>صفحه اصلی</Link></li>
+            <li className='list-group-item'><Link to='/' className='link-st'>مشخصات قالب</Link></li>
+            <li className='list-group-item'><Link to='/' className='link-st'>فروشگاه</Link></li>
+            <li className='list-group-item'><Link to='/' className='link-st'>سوالات متداول</Link></li>
+            <li className='list-group-item'><Link to='/' className='link-st'>شگفت انگیزشو</Link></li>
+            <li className='list-group-item'><Link to='/' className='link-st'>وبلاگ</Link></li>
+          </ul>
         <div className='mx-3' style={{ width: '1px', height: '50%', backgroundColor: '#1e36bd30' }} />
         <div className='d-flex justify-content-center align-items-center'>
           <Link to='/' className='link-st'>دسته بندی محصولات</Link>
