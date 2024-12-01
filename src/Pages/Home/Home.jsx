81924a8d8860001d468dd2251e9440b90452025a
78 mmd 69 <mmdqurani@gmail.com>
2024-12-01 20:59:17 +0330

    Calling the TrendSlider component, which is built for laptop sizes and up.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Story from '../../Component/Storys/Storys'
+import TrendSlider from '../../Component/TrendSlider/TrendSlider'
 
 function Home() {
   return (
     <main className=''>
       <Story />
+      <TrendSlider />
     </main>
   )
 }
