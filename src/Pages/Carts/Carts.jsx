320b65cda8c7018b902c055a437c9fc37d613d56
78 mmd 69 <mmdqurani@gmail.com>
2024-12-01 10:36:40 +0330

    Create Offers and Carts page

---
+++ src/Pages/Carts/Carts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Carts() {
+  return (
+    <main>Carts</main>
+  )
+}
+
+export default Carts
---
+++ src/Pages/Offers/Offers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Offers() {
+  return (
+    <main>Offers</main>
+  )
+}
+
+export default Offers
