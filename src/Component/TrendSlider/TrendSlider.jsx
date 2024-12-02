5fb6b055bf68175aa9882c58438ec640100a9d41
78 mmd 69 <mmdqurani@gmail.com>
2024-12-02 22:33:09 +0330

    Making the trend Slider component responsive

--- src/Component/TrendSlider/TrendSlider.jsx
+++ src/Component/TrendSlider/TrendSlider.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css';
@@ -6,6 +6,24 @@ import './TrendSlider.css'
 import { Link } from 'react-router-dom';
 
 function TrendSlider() {
+
+  const [trendSrc , setTrendSrc] = useState('./public/assets/img/Trend/newSlider3-1.jpg');
+
+  const resizeHandler = () => {
+    if(window.innerWidth < 990) {
+      setTrendSrc('./public/assets/img/Trend/mob4-1.jpg');
+    }else {
+      setTrendSrc('./public/assets/img/Trend/newSlider3-1.jpg');
+    }
+  }
+
+  useEffect(() => {
+
+    resizeHandler()
+
+    window.addEventListener('resize' , resizeHandler);
+  })
+
   return (
     <>
       <Swiper
@@ -17,23 +35,23 @@ function TrendSlider() {
         slidesPerView={1}
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
-        style={{ height: '26rem' }}
+        style={{ height: 'max-content' }}
         className='py-3'
       >
 
         <SwiperSlide style={{ height: '100%' }} >
           <Link to='/shop'>
-            <img className='trend-img-st' src="./public/assets/img/Trend/newSlider2-1.jpg" alt="" />
+            <img className='trend-img-st' src={trendSrc} alt="" />
           </Link>
         </SwiperSlide>
         <SwiperSlide>
           <Link to='/shop'>
-            <img className='trend-img-st' src="./public/assets/img/Trend/newSlider3-1.jpg" alt="" />
+            <img className='trend-img-st' src={trendSrc} alt="" />
           </Link>
         </SwiperSlide>
         <SwiperSlide>
           <Link to='/shop'>
-            <img className='trend-img-st' src="./public/assets/img/Trend/newSlider4-1.jpg" alt="" />
+            <img className='trend-img-st' src={trendSrc} alt="" />
           </Link>
         </SwiperSlide>
 
