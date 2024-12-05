8a727716c29f10c9ce2a21e6efbaaa6a6489da8f
78 mmd 69 <mmdqurani@gmail.com>
2024-12-05 14:44:06 +0330

    Refactoring the TrendCardSlider component and changing the component style

--- src/Component/TrendSlider/TrendCardSlider/TrendCardSlider.jsx
+++ src/Component/TrendSlider/TrendCardSlider/TrendCardSlider.jsx
@@ -1,31 +1,49 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Scrollbar, Autoplay , EffectCards } from 'swiper/modules';
+import { Scrollbar, Autoplay, EffectCreative } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/effect-cards';
+import 'swiper/css/effect-creative';
+import './TrendCardSlider.css'
+import { Link } from 'react-router-dom';
 
 function TrendCardSlider() {
   return (
     <>
       <Swiper
         className='h-100'
-        modules={[EffectCards , Scrollbar , Autoplay]}
-        effect={'cards'}
+        modules={[EffectCreative, Scrollbar, Autoplay]}
         grabCursor={true}
+        effect={'creative'}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+          },
+          next: {
+            translate: ['100%', 0, 0],
+          },
+        }}
         loop={true}
         autoplay={{
           delay: 4500, // فاصله بین اسلایدها به میلی‌ثانیه  
           disableOnInteraction: false, // متوقف نشود با تعاملات کاربر  
         }}
+        dir='ltr'
       >
-        <SwiperSlide className='bg-warning' style={{borderRadius:'1rem'}}>
-          <img src="./public/assets/img/Trend/mob5-1.jpg" style={{ objectFit: 'cover', height: '100%', width: '100%' }} alt="" />
+        <SwiperSlide className='bg-warning border-parent-trend-card-slider'>
+          <Link to='/'>
+            <img src="./public/assets/img/Trend/mob2-1.jpg" className='img-trend-card-slider' alt="" />
+          </Link>
         </SwiperSlide>
-        <SwiperSlide className='bg-warning' style={{borderRadius:'1rem'}}>
-          <img src="./public/assets/img/Trend/mob2-1.jpg" style={{ objectFit: 'cover', height: '100%', width: '100%' }} alt="" />
+        <SwiperSlide className='bg-warning border-parent-trend-card-slider'>
+          <Link to='/'>
+            <img src="./public/assets/img/Trend/mob3-1.jpg" className='img-trend-card-slider' alt="" />
+          </Link>
         </SwiperSlide>
-        <SwiperSlide className='bg-warning' style={{borderRadius:'1rem'}}>
-          <img src="./public/assets/img/Trend/mob4-1.jpg" style={{ objectFit: 'cover', height: '100%', width: '100%' }} alt="" />
+        <SwiperSlide className='bg-warning border-parent-trend-card-slider'>
+          <Link to='/'>
+            <img src="./public/assets/img/Trend/mob5-1.jpg" className='img-trend-card-slider' alt="" />
+          </Link>
         </SwiperSlide>
       </Swiper>
     </>
