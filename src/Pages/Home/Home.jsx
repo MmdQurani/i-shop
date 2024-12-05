dc789f4bb96884d0a6b21eb203a8d0e28e81312f
78 mmd 69 <mmdqurani@gmail.com>
2024-12-05 21:06:22 +0330

    First call to the HeadSection component in the parent component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Story from '../../Component/Storys/Storys'
 import TrendSlider from '../../Component/TrendSlider/TrendSlider'
+import HeadSection from '../../Component/HeadSection/HeadSection'
 
 function Home() {
   return (
     <main className=''>
       <Story />
       <TrendSlider />
+      <HeadSection />
     </main>
   )
 }
