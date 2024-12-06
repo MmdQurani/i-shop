25c4a0fc8e3254877d0f46c18800df21fb66524a
78 mmd 69 <mmdqurani@gmail.com>
2024-12-06 16:43:54 +0330

    Responsiveness of PopularCategories component items

--- src/Component/PopularCategories/PopularCategories.jsx
+++ src/Component/PopularCategories/PopularCategories.jsx
@@ -4,6 +4,7 @@ import HeadSection from '../HeadSection/HeadSection'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Navigation } from 'swiper/modules';
+import { FaChevronRight , FaChevronLeft } from "react-icons/fa";
 
 function PopularCategories() {
   return (
@@ -11,7 +12,7 @@ function PopularCategories() {
       <HeadSection moreItems={true} title={`دسته بندی های محبوب`} link='/' />
 
       <div className='parent-swiper-box'>
-        <div className='bg-danger h-100 mx-swiper-box'>
+        <div className='h-100 mx-swiper-box'>
           <Swiper
             slidesPerView={9}
             modules={[Navigation]}
@@ -19,28 +20,50 @@ function PopularCategories() {
               nextEl:'.swiper-next-button',
               prevEl:'.swiper-prev-button'
             }}
+            spaceBetween={15}
+            breakpoints={{
+              320: {
+                slidesPerView: 1, // یک اسلاید برای صفحه نمایش کوچک  
+              },
+              410: {
+                slidesPerView: 2, // یک اسلاید برای صفحه نمایش کوچک  
+              },
+              768: {
+                slidesPerView: 3, // یک اسلاید برای صفحه نمایش کوچک  
+              },
+              992: {
+                slidesPerView: 6, // سه اسلاید برای صفحه نمایش بزرگ  
+              },
+              1400: {
+                slidesPerView: 9, // سه اسلاید برای صفحه نمایش بزرگ  
+              }
+            }}
             style={{height:'10rem' , overflow:'hidden'}}
           >
 
             <SwiperSlide className='bg-warning h-100'>
               <img src="./public/assets/img/Trend/mob4-1.jpg" style={{objectFit:'cover' , width:'100%'}} alt="" />
             </SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 2</SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 3</SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 4</SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 5</SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 6</SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 7</SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 8</SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 9</SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 10</SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 11</SwiperSlide>
-            <SwiperSlide className='bg-warning h-100'>slide 12</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 2</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 3</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 4</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 5</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 6</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 7</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 8</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 9</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 10</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 11</SwiperSlide>
+            <SwiperSlide className='bg-warning rounded-3 h-100'>slide 12</SwiperSlide>
           </Swiper>
         </div>
         <div dir='rtl' className='navigation-btn-st d-flex flex-row justify-content-between'>
-          <button className='swiper-next-button'>next</button>
-          <button className='swiper-prev-button'>prev</button>
+          <button className='swiper-next-button'>
+            <FaChevronRight className='icon-swiper' />
+          </button>
+          <button className='swiper-prev-button'>
+            <FaChevronLeft className='icon-swiper' />
+          </button>
         </div>
       </div>
     </div>
