edda8af1a7c7a4e573c4f2f3c4738963d3d7a25b
78 mmd 69 <mmdqurani@gmail.com>
2024-12-07 20:19:13 +0330

    Create a BtnCards component for the BoxCards component

---
+++ src/Component/BtnCards/BtnCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import './BtnCards.css'
+
+function BtnCards() {
+  return (
+    <>
+      <Button variant="primary">Go somewhere</Button>
+    </>
+  )
+}
+
+export default BtnCards
