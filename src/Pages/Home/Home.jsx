9f94008ff10b26879db6cfb4e5c3c2252b7d2eb0
78 mmd 69 <mmdqurani@gmail.com>
2024-12-11 20:00:04 +0330

    Calling Best Sellers, New Product

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import Story from '../../Component/Storys/Storys'
 import TrendSlider from '../../Component/TrendSlider/TrendSlider'
 import PopularCategories from '../../Component/PopularCategories/PopularCategories'
-import ProductSlide from '../../Component/ProductSlide/ProductSlide'
-import { SwiperSlide } from 'swiper/react'
-import BoxCards from '../../Component/BoxCards/BoxCards'
+import NewProduct from '../../Component/NewProduct/NewProduct'
+import Bestsellers from '../../Component/Bestsellers/Bestsellers'
 
 function Home() {
   return (
@@ -12,55 +11,8 @@ function Home() {
       <Story />
       <TrendSlider />
       <PopularCategories />
-
-      <ProductSlide HeadSection='محصولات جدید'>
-
-        <SwiperSlide className=' my-auto d-flex align-items-center'>
-          <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
-        </SwiperSlide>
-
-        <SwiperSlide className=' my-auto d-flex align-items-center'>
-          <BoxCards src='./assets/img/Products/4-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
-        </SwiperSlide>
-
-        <SwiperSlide className=' my-auto d-flex align-items-center'>
-          <BoxCards src='./assets/img/Products/8-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
-        </SwiperSlide>
-
-        <SwiperSlide className=' my-auto d-flex align-items-center'>
-          <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
-        </SwiperSlide>
-
-        <SwiperSlide className=' my-auto d-flex align-items-center'>
-          <BoxCards src='./assets/img/Products/4-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
-        </SwiperSlide>
-
-      </ProductSlide>
-
-      <ProductSlide HeadSection='پرفورش ترین ها'>
-
-        <SwiperSlide className=' my-auto d-flex align-items-center'>
-          <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
-        </SwiperSlide>
-
-        <SwiperSlide className=' my-auto d-flex align-items-center'>
-          <BoxCards src='./assets/img/Products/4-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
-        </SwiperSlide>
-
-        <SwiperSlide className=' my-auto d-flex align-items-center'>
-          <BoxCards src='./assets/img/Products/8-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
-        </SwiperSlide>
-
-        <SwiperSlide className=' my-auto d-flex align-items-center'>
-          <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
-        </SwiperSlide>
-
-        <SwiperSlide className=' my-auto d-flex align-items-center'>
-          <BoxCards src='./assets/img/Products/4-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
-        </SwiperSlide>
-
-      </ProductSlide>
-
+      <NewProduct />
+      <Bestsellers />
     </main>
   )
 }
