5a920a472eed79ba162900691b10fa44342c559d
78 mmd 69 <mmdqurani@gmail.com>
2024-12-11 12:05:53 +0330

    Calling the BannerNewProduct component inside the parent component

--- src/Component/NewProducts/NewProducts.jsx
+++ src/Component/NewProducts/NewProducts.jsx
@@ -8,87 +8,89 @@ import { Card, Button } from 'react-bootstrap'
 import './NewProducts.css'
 import 'swiper/css';
 import BoxCards from '../BoxCards/BoxCards'
+import BannerNewProduct from './BannerNewProduct/BannerNewProduct'
 
 function NewProducts() {
   return (
 
-    <div>
-      <HeadSection moreItems={true} link='/' title='محصولات جدید' />
+    <>
+      <div>
+        <HeadSection moreItems={true} link='/' title='محصولات جدید' />
 
-      <div className='parent-swiper-box'>
-        <div className='h-100 mx-swiper-box'>
-          <Swiper
-            modules={[Navigation]}
-            navigation={{
-              nextEl: '#NewProducts-next',
-              prevEl: '#NewProducts-prev'
-            }}
-            spaceBetween={20}
-            loop={true}
-            breakpoints={{
-              100: {
-                slidesPerView: 1, // یک اسلاید برای صفحه نمایش کوچک  
-              },
-              440: {
-                slidesPerView: 1.2, // یک اسلاید برای صفحه نمایش کوچک  
-              },
-              520: {
-                slidesPerView: 1.5, // یک اسلاید برای صفحه نمایش کوچک  
-              },
-              800: {
-                slidesPerView: 1.8, // یک اسلاید برای صفحه نمایش کوچک  
-              },
-              992: {
-                slidesPerView: 2.7, // سه اسلاید برای صفحه نمایش بزرگ  
-              },
-              1024: {
-                slidesPerView: 2.7, // سه اسلاید برای صفحه نمایش بزرگ  
-              },
-              1200: {
-                slidesPerView: 3, // سه اسلاید برای صفحه نمایش بزرگ  
-              },
-              1400: {
-                slidesPerView: 3.7, // سه اسلاید برای صفحه نمایش بزرگ  
-              },
-              1700: {
-                slidesPerView: 4, // سه اسلاید برای صفحه نمایش بزرگ  
-              }
-            }}
-            style={{ height: '25rem', overflow: 'hidden' }}
-            className='px-2'
-          >
+        <div className='parent-swiper-box'>
+          <div className='h-100 mx-swiper-box'>
+            <Swiper
+              modules={[Navigation]}
+              navigation={{
+                nextEl: '#NewProducts-next',
+                prevEl: '#NewProducts-prev'
+              }}
+              spaceBetween={20}
+              loop={true}
+              breakpoints={{
+                100: {
+                  slidesPerView: 1, // یک اسلاید برای صفحه نمایش کوچک  
+                },
+                440: {
+                  slidesPerView: 1.2, // یک اسلاید برای صفحه نمایش کوچک  
+                },
+                520: {
+                  slidesPerView: 1.5, // یک اسلاید برای صفحه نمایش کوچک  
+                },
+                800: {
+                  slidesPerView: 1.8, // یک اسلاید برای صفحه نمایش کوچک  
+                },
+                992: {
+                  slidesPerView: 2.7, // سه اسلاید برای صفحه نمایش بزرگ  
+                },
+                1024: {
+                  slidesPerView: 2.7, // سه اسلاید برای صفحه نمایش بزرگ  
+                },
+                1200: {
+                  slidesPerView: 3, // سه اسلاید برای صفحه نمایش بزرگ  
+                },
+                1400: {
+                  slidesPerView: 3.7, // سه اسلاید برای صفحه نمایش بزرگ  
+                },
+                1700: {
+                  slidesPerView: 4, // سه اسلاید برای صفحه نمایش بزرگ  
+                }
+              }}
+              style={{ height: '25rem', overflow: 'hidden' }}
+              className='px-2'
+            >
 
-            <SwiperSlide className=' my-auto d-flex align-items-center'>
-              <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000'/>
+              <SwiperSlide className=' my-auto d-flex align-items-center'>
+                <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
 
-            </SwiperSlide>
-            <SwiperSlide className=' my-auto d-flex align-items-center'>
-              <BoxCards src='./assets/img/Products/4-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000'/>
+              </SwiperSlide>
+              <SwiperSlide className=' my-auto d-flex align-items-center'>
+                <BoxCards src='./assets/img/Products/4-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
 
-            </SwiperSlide>
-            <SwiperSlide className=' my-auto d-flex align-items-center'>
-              <BoxCards src='./assets/img/Products/8-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000'/>
+              </SwiperSlide>
+              <SwiperSlide className=' my-auto d-flex align-items-center'>
+                <BoxCards src='./assets/img/Products/8-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
 
-            </SwiperSlide>
-            <SwiperSlide className=' my-auto d-flex align-items-center'>
-              <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000'/>
+              </SwiperSlide>
+              <SwiperSlide className=' my-auto d-flex align-items-center'>
+                <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
 
-            </SwiperSlide>
-            <SwiperSlide className=' my-auto d-flex align-items-center'>
-              <BoxCards src='./assets/img/Products/4-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000'/>
+              </SwiperSlide>
+              <SwiperSlide className=' my-auto d-flex align-items-center'>
+                <BoxCards src='./assets/img/Products/4-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
 
-            </SwiperSlide>
+              </SwiperSlide>
 
-          </Swiper>
+            </Swiper>
+          </div>
+
+          <NavigationBtnCard prev='NewProducts-prev' next='NewProducts-next' />
+
         </div>
 
-        <NavigationBtnCard prev='NewProducts-prev' next='NewProducts-next' />
-
+        <BannerNewProduct />
       </div>
-    </div>
-
-
-
+    </>
   )
 }
 
