aa6772bfc0e66089f9df8bbc1ea534d4fe973196
78 mmd 69 <mmdqurani@gmail.com>
2024-12-14 18:24:33 +0330

    Creating a Amazing Component

---
+++ src/Component/Amazing/Amazing.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ProductSlide from '../ProductSlide/ProductSlide'
+import { SwiperSlide } from 'swiper/react'
+import BoxCards from '../BoxCards/BoxCards'
+
+function Amazing() {
+  return (
+    <>
+      <ProductSlide HeadSection='محصولات شگفت انگیز' Amazing={true}>
+
+        <SwiperSlide className=' my-auto d-flex align-items-center'>
+          <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+        </SwiperSlide>
+
+        <SwiperSlide className=' my-auto d-flex align-items-center'>
+          <BoxCards src='./assets/img/Products/4-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+        </SwiperSlide>
+
+        <SwiperSlide className=' my-auto d-flex align-items-center'>
+          <BoxCards src='./assets/img/Products/8-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+        </SwiperSlide>
+
+        <SwiperSlide className=' my-auto d-flex align-items-center'>
+          <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+        </SwiperSlide>
+
+        <SwiperSlide className=' my-auto d-flex align-items-center'>
+          <BoxCards src='./assets/img/Products/4-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+        </SwiperSlide>
+
+      </ProductSlide>
+    </>
+  )
+}
+
+export default Amazing
