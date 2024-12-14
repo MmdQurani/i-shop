a58b3c1d5ab004fab30678966177f6c03a246a47
78 mmd 69 <mmdqurani@gmail.com>
2024-12-14 18:29:31 +0330

    Calling components created in the parent component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,10 @@ import TrendSlider from '../../Component/TrendSlider/TrendSlider'
 import PopularCategories from '../../Component/PopularCategories/PopularCategories'
 import NewProduct from '../../Component/NewProduct/NewProduct'
 import Bestsellers from '../../Component/Bestsellers/Bestsellers'
+import Mobile from '../../Component/Mobile/Mobile'
+import Laptop from '../../Component/Laptop/Laptop'
+import Amazing from '../../Component/Amazing/Amazing'
+import ApprovedProducts from '../../Component/ApprovedProducts/ApprovedProducts'
 
 function Home() {
   return (
@@ -12,7 +16,11 @@ function Home() {
       <TrendSlider />
       <PopularCategories />
       <NewProduct />
+      <Mobile />
       <Bestsellers />
+      <Amazing />
+      <ApprovedProducts />
+      <Laptop />
     </main>
   )
 }
