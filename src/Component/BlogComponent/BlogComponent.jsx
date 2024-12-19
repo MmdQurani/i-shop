b5b3688d6caf88cce837c66b930023bdd58c77a7
78 mmd 69 <mmdqurani@gmail.com>
2024-12-19 13:29:33 +0330

    Basic blog banner template

--- src/Component/BlogComponent/BlogComponent.jsx
+++ src/Component/BlogComponent/BlogComponent.jsx
@@ -11,6 +11,15 @@ function BlogComponent() {
           <div className="col-xl-7 col-12">
             <div className='banner-blog-main position-relative w-100 h-100'>
               <img src="./assets/img/BlogImg/pci1.png" style={{height:'100%' , width:'100%' , borderRadius:'1rem'}} alt="" />
+              <div style={{position:'absolute' , zIndex:100}} className='w-100 h-100 top-0 d-flex flex-column justify-content-end row-gap-3 px-4'>
+                <span className='text-light' >دلایلی برای خرید گوشی ناتینگ فون یک</span>
+                <div className='d-flex flex-row w-100 text-light column-gap-5 mb-4'>
+                  <span>مدیر سایت</span>
+                  <span dir='rtl'>06مهر1403</span>
+                  <span>1</span>
+                  <span>15 دقیقه</span>
+                </div>
+              </div>
             </div>
           </div>
           <div className="col-xl-5 col-12 bg-danger">one</div>
