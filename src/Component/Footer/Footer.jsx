067e9bc303d274491a6a95793f08f076104bbd0b
78 mmd 69 <mmdqurani@gmail.com>
2024-12-23 20:18:31 +0330

    Making the Footer Component Dynamic

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -10,61 +10,45 @@ function Footer() {
 
           <div className="col-xl-5 col-12 pe-0 d-flex justify-content-center">
             <div className="row row-gap-5 w-100">
-              <div className="col-xl-4 col-6 p-0">
-                <div className='w-100 h-100'>
-                  <TitleSiteFooter title='دسترسی های سریع' />
-                  <div className='d-flex flex-column row-gap-4 mt-4'>
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                  </div>
-                </div>
-              </div>
-              <div className="col-xl-3 col-6 p-0">
-                <div className='w-100 h-100'>
-                  <TitleSiteFooter title='برندهای مطرح' />
-                  <div className='d-flex flex-column row-gap-4 mt-4'>
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                  </div>
-                </div>
-              </div>
-              <div className="col-xl-3 col-6 p-0">
-                <div className='w-100 h-100'>
-                  <TitleSiteFooter title='راهنمای مشتریان' />
-                  <div className='d-flex flex-column row-gap-4 mt-4'>
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                  </div>
-                </div>
-              </div>
-              <div className="col-xl-2 col-6 p-0">
-                <div className='w-100 h-100'>
-                  <TitleSiteFooter title='دسته بندی ها' />
-                  <div className='d-flex flex-column row-gap-4 mt-4'>
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                    <ItemSiteFooter item='صفحه اصلی' />
-                  </div>
-                </div>
-              </div>
+              <SiteFooter title='دسترسی های سریع' col='col-xl-4'>
+                <ItemSiteFooter item='صفحه اصلی' />
+                <ItemSiteFooter item='صفحه اصلی' />
+                <ItemSiteFooter item='صفحه اصلی' />
+                <ItemSiteFooter item='صفحه اصلی' />
+                <ItemSiteFooter item='صفحه اصلی' />
+                <ItemSiteFooter item='صفحه اصلی' />
+              </SiteFooter>
+              <SiteFooter title='برندهای مطرح'>
+                <ItemSiteFooter item='شیائومی' />
+                <ItemSiteFooter item='شیائومی' />
+                <ItemSiteFooter item='شیائومی' />
+                <ItemSiteFooter item='شیائومی' />
+                <ItemSiteFooter item='شیائومی' />
+                <ItemSiteFooter item='شیائومی' />
+              </SiteFooter>
+              <SiteFooter title='راهنمای مشتریان'>
+                <ItemSiteFooter item='سبد خرید' />
+                <ItemSiteFooter item='سبد خرید' />
+                <ItemSiteFooter item='سبد خرید' />
+                <ItemSiteFooter item='سبد خرید' />
+                <ItemSiteFooter item='سبد خرید' />
+                <ItemSiteFooter item='سبد خرید' />
+              </SiteFooter>
+              <SiteFooter title='دسته بندی ها' col='col-xl-2'>
+                <ItemSiteFooter item='موبایل' />
+                <ItemSiteFooter item='موبایل' />
+                <ItemSiteFooter item='موبایل' />
+                <ItemSiteFooter item='موبایل' />
+                <ItemSiteFooter item='موبایل' />
+                <ItemSiteFooter item='موبایل' />
+              </SiteFooter>
             </div>
           </div>
 
-          <div className="col-xl-4 col-6">
+          <div className="col-xl-4 col-sm-6">
             <TitleSiteFooter title='پرفروش ترین محصولات' />
           </div>
-          <div className="col-xl-3 col-6">
+          <div className="col-xl-3 col-sm-6">
             <TitleSiteFooter title='چگونه به مــــــا اعتماد کنیم' />
           </div>
         </div>
@@ -73,11 +57,24 @@ function Footer() {
   )
 }
 
+export function SiteFooter(prop) {
+  return (
+    <div className={`${prop.col ? prop.col : 'col-xl-3'} col-sm-6 col-12 p-0`}>
+      <div className='w-100 h-100'>
+        <TitleSiteFooter title={prop.title} />
+        <div className='d-flex flex-column row-gap-4 mt-4'>
+          {prop.children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export function TitleSiteFooter(prop) {
   return (
     <>
       <div className='position-relative pb-3'>
-        <span className='TitleSiteFooter sub-text-color-main w-100 d-flex w-100' style={{fontSize:'14px'}}>{prop.title}</span>
+        <span className='TitleSiteFooter sub-text-color-main w-100 d-flex w-100' style={{ fontSize: '14px' }}>{prop.title}</span>
       </div>
     </>
   )
@@ -85,7 +82,7 @@ export function TitleSiteFooter(prop) {
 
 export function ItemSiteFooter(prop) {
   return (
-    <Link to='/' className='text-decoration-none text-color-main ItemSiteFooter' style={{ width: 'max-content' , fontSize:'12px' }}>{prop.item}</Link>
+    <Link to='/' className='text-decoration-none text-color-main ItemSiteFooter' style={{ width: 'max-content', fontSize: '12px' }}>{prop.item}</Link>
   )
 }
 
