425c6938494d830f0b034ef01ba64b836489e7a9
78 mmd 69 <mmdqurani@gmail.com>
2025-01-11 19:15:54 +0330

    Creating the SideRightCategory component and also making the components inside the Category component responsive

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ArchiveContentTitle from '../../Component/ArchiveContentTitle/ArchiveContentTitle'
+import SideRightCategory from '../../Component/SideRightCategory/SideRightCategory'
 
 function Category() {
   return (
-    <main className='bg-main'>
+    <main className='Category bg-main'>
       <ArchiveContentTitle />
+
+      <div className='content-category row' dir='rtl'>
+
+        <div className="col-xxl-3 col-xl-4 col-lg-4 p-0 d-lg-flex d-none">
+          <SideRightCategory />
+        </div>
+        <div className="col-xxl-9 col-xl-8 col-lg-8 col-12">
+          <div className='bg-danger'>content</div>
+        </div>
+
+      </div>
     </main>
   )
 }
