0d5e7245003133f924109b0f7a8f4793715a6158
78 mmd 69 <mmdqurani@gmail.com>
2025-01-15 21:42:58 +0330

    Fixed some bugs related to the header section.

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -6,7 +6,7 @@ import './Header.css'
 
 function Header() {
   return (
-    <header className='headerSize bg-main' style={{zIndex:'100'}}>
+    <header className='headerSize bg-main' style={{zIndex:'1000'}}>
       <Container className='p-0 d-flex flex-column h-100'>
         <Topbar />
         <div className='after-item py-4'></div>
