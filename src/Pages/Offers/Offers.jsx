b8b0e26f4880b35e89414e218040ebba91a40c2c
78 mmd 69 <mmdqurani@gmail.com>
2025-01-16 13:40:58 +0330

    Using the BoxCards component in the Offers component and making it responsive for all sizes

--- src/Pages/Offers/Offers.jsx
+++ src/Pages/Offers/Offers.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import './Offers.css'
+import ArchiveContentTitle from '../../Component/ArchiveContentTitle/ArchiveContentTitle'
+import BoxCards from '../../Component/BoxCards/BoxCards'
 
 function Offers() {
   return (
-    <div>Offers</div>
+    <>
+      <div className='Offers-shop'>
+        <ArchiveContentTitle pageName='شگفت انگیزشو' countItem='13 محصول' />
+
+        <div className="Offers-Products w-100 h-100 mt-4">
+          <div className="row row-gap-4 justify-content-center align-items-center px-2">
+
+            <div className="col-xxl-3 col-lg-4 col-sm-6 col-10">
+              <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+            </div>
+            <div className="col-xxl-3 col-lg-4 col-sm-6 col-10">
+              <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+            </div>
+            <div className="col-xxl-3 col-lg-4 col-sm-6 col-10">
+              <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+            </div>
+            <div className="col-xxl-3 col-lg-4 col-sm-6 col-10">
+              <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+            </div>
+
+            <div className="col-xxl-3 col-lg-4 col-sm-6 col-10">
+              <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+            </div>
+            <div className="col-xxl-3 col-lg-4 col-sm-6 col-10">
+              <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+            </div>
+            <div className="col-xxl-3 col-lg-4 col-sm-6 col-10">
+              <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+            </div>
+            <div className="col-xxl-3 col-lg-4 col-sm-6 col-10">
+              <BoxCards src='./assets/img/Products/2-150x150.png' title='گوشی موبایل اپل مدل پرومکس دو سیم‌ کارت نات اکتیو سیزده' price='55,000,000' />
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </>
   )
 }
 
