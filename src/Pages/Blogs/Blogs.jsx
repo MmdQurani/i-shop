5644cf93280f647f35e4519c551e87f9d35e0f7b
78 mmd 69 <mmdqurani@gmail.com>
2025-01-19 16:10:52 +0330

    Completing the blog-latest component

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react'
 import './Blogs.css'
 import ArchiveContentTitle from '../../Component/ArchiveContentTitle/ArchiveContentTitle'
 import BannerSlider from '../../Component/BannerSlider/BannerSlider'
+import HeadSection from '../../Component/HeadSection/HeadSection';
+import { LuUser } from 'react-icons/lu';
+import { MdOutlineDateRange } from 'react-icons/md';
+import { FiMessageSquare } from 'react-icons/fi';
+import { FaRegClock } from 'react-icons/fa';
 
 function Blogs() {
 
@@ -38,18 +43,114 @@ function Blogs() {
       <div className='Blogs-Content'>
         <ArchiveContentTitle pageName='مقاله ها' countItem='15 مقاله' />
 
-        <div className="Blogs-Items w-100 h-100">
+        <div className="Blogs-Banners w-100 h-100">
+          <BannerSlider items={blogBanner} />
+        </div>
 
-          <div className='row'>
+        <div className='blog-latest mt-5 d-flex flex-column justify-content-start'>
+          <HeadSection moreItems={false} link='/' title={`جدیدترین مطالب وبلاگ`} />
 
-            <div className='col-12'>
-              <BannerSlider items={blogBanner} />
+          <div className="row w-100 h-100 justify-content-center mx-auto row-gap-4 mt-2" dir='rtl'>
+            <div className="col-xl-6 col-md-6 col-12">
+              <BlogBannerLatest />
             </div>
-            
+
+            <div className="col-xl-3 col-md-6 col-12">
+              <BlogCard />
+            </div>
+            <div className="col-xl-3 col-md-6 col-12">
+              <BlogCard />
+            </div>
+            <div className="col-xl-3 col-md-6 col-12">
+              <BlogCard />
+            </div>
+            <div className="col-xl-3 col-md-6 col-12">
+              <BlogCard />
+            </div>
+
+            <div className="col-xl-6 col-md-6 col-12">
+              <BlogBannerLatest />
+            </div>
           </div>
 
         </div>
 
+      </div>
+    </>
+  )
+}
+
+export function BlogBannerLatest() {
+  return (
+    <>
+      <div className='banner-blog-main position-relative w-100 h-100'>
+        <img src="./assets/img/BlogImg/mag3.jpg" className='img-banner-main-blog' alt="" />
+        <div style={{ zIndex: 100 }} className='position-absolute w-100 h-100 top-0 d-flex flex-column justify-content-end row-gap-3 px-2 pb-3'>
+          <span className='text-light fs-5 pe-3 title-banner-main-blog' >دلایلی برای خرید گوشی ناتینگ فون یک</span>
+          <div className='d-flex flex-row justify-content-center w-100 text-light column-gap-3 fs-xs-banner-blog'>
+
+            <div className="blog-banner-latest row w-100 row-gap-2">
+
+              <div className='col-xxl-3 col-6'>
+                <span>
+                  <LuUser className='ms-1' />
+                  مدیر سایت
+                </span>
+              </div>
+              <div className='col-xxl-3 col-6'>
+                <div dir='rtl' className='d-flex flex-row column-gap-1 align-items-center'>
+                  <MdOutlineDateRange fontSize={'16px'} />
+                  <span>06</span>
+                  <span>مهر</span>
+                  <span>1403</span>
+                </div>
+              </div>
+              <div className='col-xxl-2 col-6'>
+                <span>
+                  <FiMessageSquare className='ms-1' />
+                  1
+                </span>
+              </div>
+              <div className='col-xxl-3 col-6'>
+                <div className='d-flex flex-row column-gap-1 align-items-center'>
+                  <FaRegClock />
+                  <span>15</span>
+                  <span>دقیقه</span>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export function BlogCard() {
+  return (
+    <>
+      <div className='blog-card w-100 h-100'>
+        <div class="card w-100 h-100">
+          <img src="./assets/img/BlogImg/pci1.png" class="card-img-top" alt="" />
+          <div class="card-body py-4 px-3 row-gap-3 d-flex flex-column justify-content-center align-items-start">
+            <h5 class="card-title fs-6 text-color-main fs-6 text-color-main">دلایلی برای خرید گوشی ناتینگ فون یک</h5>
+            <p className="card-text lh-lg sub-text-color-main " style={{ fontSize: '14px' }}>
+              دلایل خرید گوشی ۱ Nothing Phone بسیار قانع‌کننده‌ هستند، با توجه به نوع طراحی پیشرو، سخت‌افزار قدرتمند و قیمت مناسب، گوشی ناتینگ دلایل کافی برای خرید را در اختیار کاربر قرار می‌دهد.
+            </p>
+            <div className="d-flex w-100 h-100 flex-row justify-content-between align-items-center px-2 text-color-main" style={{ fontSize: '13px' }}>
+              <div className='d-flex flex-row column-gap-1 align-items-center'>
+                <FaRegClock />
+                <span className='me-1'>15</span>
+                <span>دقیقه</span>
+              </div>
+              <span>
+                <FiMessageSquare className='ms-1' />
+                1
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
