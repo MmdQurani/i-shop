8e8a22888af9b89e32a9be4f8ba5abb126d74350
78 mmd 69 <mmdqurani@gmail.com>
2025-01-24 17:47:05 +0330

    Get product information clicked by the user

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useParams } from 'react-router-dom'
+import data from '../../Datas';
 
 function Product() {
+  const [productItem , setProductItem] = useState(data);
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState(location.pathname);
+  const {productParams} = useParams();
+  const [productIndex , setProductIndex] = useState([])
+
+  useEffect( () => {
+    setProductIndex(productItem.filter(item => item.id == productParams))
+  } , [])
+
   return (
-    <div>Product</div>
+    <>
+    <div className='product'>
+      {productIndex.map(item => {
+        return(
+          <>
+          <div>{item.name}</div>
+          </>
+        )
+      })}
+    </div>
+    </>
   )
 }
 
