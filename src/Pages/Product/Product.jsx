7a1874ed79c8a4ab982819344395228a5de84ab7
78 mmd 69 <mmdqurani@gmail.com>
2025-01-26 19:40:06 +0330

    Division of available space for displaying the product and its information

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -22,6 +22,17 @@ function Product() {
 
             <div className='product'>
               <ArchiveContentTitle pageName={item.name} />
+
+              <div className="product-box bg-main card py-4 px-4 mt-4">
+                <div className="row" dir='rtl'>
+                  <div className="col-5">
+                    <div className='bg-danger'>two</div>
+                  </div>
+                  <div className="col-7">
+                    <div className='bg-danger'>one</div>
+                  </div>
+                </div>
+              </div>
             </div>
 
           </>
