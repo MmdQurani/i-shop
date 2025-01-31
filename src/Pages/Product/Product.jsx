b4604e33a32bc16c58a45ce473d0bfefcd7c0457
78 mmd 69 <mmdqurani@gmail.com>
2025-01-31 12:38:57 +0330

    Responsiveness of the Subsamples-img-parent component

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -26,9 +26,9 @@ function Product() {
               <ArchiveContentTitle pageName={item.name} />
 
               <div className="product-box bg-main card py-4 px-4 mt-4">
-                <div className="row" dir='rtl'>
-                  <div className="col-5">
-                    <div className='bg-main d-flex justify-content-center align-items-center w-100 h-100'>
+                <div className="row row-gap-5" dir='rtl'>
+                  <div className="col-lg-5 col-12">
+                    <div className='bg-main d-flex flex-column row-gap-4 justify-content-center align-items-center w-100 h-100'>
                       <InnerImageZoom
                         src="../assets/img/Products/4.png"
                         zoomSrc="../assets/img/Products/4.png"
@@ -37,9 +37,42 @@ function Product() {
                         zoomScale={1.8}
                       />
 
+                      <div className='row Subsamples-img-parent column-gap-4 row-gap-3 w-100 justify-content-center'>
+                        <div className='col-xxl-2 col-xl-3 col-lg-3 col-md-2 col-sm-3 col-3 p-0'>
+                          <div className='Subsamples-img w-100 h-100 p-1 rounded-2'>
+                            <img src="../assets/img/Products/4.png" className='w-100' alt="" />
+                          </div>
+                        </div>
+                        <div className='col-xxl-2 col-xl-3 col-lg-3 col-md-2 col-sm-3 col-3 p-0'>
+                          <div className='Subsamples-img w-100 h-100 p-1 rounded-2'>
+                            <img src="../assets/img/Products/4.png" className='w-100' alt="" />
+                          </div>
+                        </div>
+                        <div className='col-xxl-2 col-xl-3 col-lg-3 col-md-2 col-sm-3 col-3 p-0'>
+                          <div className='Subsamples-img w-100 h-100 p-1 rounded-2'>
+                            <img src="../assets/img/Products/4.png" className='w-100' alt="" />
+                          </div>
+                        </div>
+                        <div className='col-xxl-2 col-xl-3 col-lg-3 col-md-2 col-sm-3 col-3 p-0'>
+                          <div className='Subsamples-img w-100 h-100 p-1 rounded-2'>
+                            <img src="../assets/img/Products/4.png" className='w-100' alt="" />
+                          </div>
+                        </div>
+                        <div className='col-xxl-2 col-xl-3 col-lg-3 col-md-2 col-sm-3 col-3 p-0'>
+                          <div className='Subsamples-img w-100 h-100 p-1 rounded-2'>
+                            <img src="../assets/img/Products/4.png" className='w-100' alt="" />
+                          </div>
+                        </div>
+                        <div className='col-xxl-2 col-xl-3 col-lg-3 col-md-2 col-sm-3 col-3 p-0'>
+                          <div className='Subsamples-img w-100 h-100 p-1 rounded-2'>
+                            <img src="../assets/img/Products/4.png" className='w-100' alt="" />
+                          </div>
+                        </div>
+                      </div>
+
                     </div>
                   </div>
-                  <div className="col-7">
+                  <div className="col-lg-7 col-12">
                     <div className='bg-danger'>one</div>
                   </div>
                 </div>
